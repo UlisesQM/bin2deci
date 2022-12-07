3212785228610759d3fea64e0f028ccfe03493c1
UlisesQM <robelquistial@gmail.com>
2022-12-07 17:23:50 -0500

    Finalizado

--- main.js
+++ main.js
@@ -1,54 +1,49 @@
 // cadenas de 8 digitos binarios
 // no utilizar matrices para contener los digitos binarios introducidos
 // solo una funcion
-
-console.log("Hola")
 // const binary = prompt("Ingrese numeros binarios")
-const $button = document.querySelector(".button")
-const $input = document.querySelector(".input")
-const $resultado = document.querySelector(".resultado")
+const $button = document.querySelector(".button");
+const $input = document.querySelector(".input");
+const $resultado = document.querySelector(".resultado");
 
-
-
-
-
-
-$input.addEventListener("keyup", e=>{
-  let value2 = e.target.value
-  const value = Number(e.key)
-
-
-  //Restriccion 1 y 0
-  if((value === 1 || value === 0)){
-    console.log("Binario")
-  }else if(e.key == "Enter" && value2.length == 8){
-    $resultado.innerHTML = "Resultado"
+$input.addEventListener("keyup", (e) => {
+  function Restriccion() {
+    if (value === 1 || value === 0) {
+      console.log("Binario");
+    } else if (e.key == "Enter" && value2.length == 8) {
+    } else if (value > 1 || value < 0 || e.key != "Enter") {
+      e.target.value = value2.substring(0, value2.length - 1);
+    }else {
+      console.error("No es numero");
+    }
   }
-  else if(value > 1 || value < 0 || e.key != "Enter"){
-    e.target.value = value2.substring(0, value2.length - 1);
-  }else{
-    console.error("No es numero")
-  }
-
-  //Enter
-  if(e.key == "Enter" && value2.length == 8){
-    //REVERSION
-    let reversion = '';
-    for(let i = value2.length-1  ;i >= 0 ; i-- ){
-      reversion += value2[i]
-      }
-    console.log(reversion)
-
-    //CALULO
-    let suma = 0
+  function calculo() {
+    let reverse = "";
     for (let i = value2.length - 1; i >= 0; i--) {
-      let operador=0
-      let indice = Number(reversion[i])
-      if(indice == true){
-        operador = 2**i
-        suma = suma + operador
+      reverse += value2[i];
+    }
+    let suma = 0;
+    for (let i = value2.length - 1; i >= 0; i--) {
+      let operador = 0;
+      let indice = Number(reverse[i]);
+      if (indice == true) {
+        operador = 2 ** i;
+        suma = suma + operador;
       }
     }
-    $resultado.innerHTML = suma
+    return suma;
   }
-})
+  //Variables
+  let value2 = e.target.value;
+  const value = Number(e.key);
+
+  Restriccion();
+
+  //RESULTADO
+  $button.addEventListener("click",e=>{
+    if (value2.length == 8 ) {
+      //CALCULO
+      $resultado.innerHTML = calculo();
+    }
+  })
+});
